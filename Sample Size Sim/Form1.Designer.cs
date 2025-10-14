4739bfd14b406be1931bc6ecba6b87f09e60f825
ncvyn <93510179+ncvyn@users.noreply.github.com>
2025-10-15 07:44:35 +0800

    Refactor and enhance form controls and event handling

--- Sample Size Sim/Form1.Designer.cs
+++ Sample Size Sim/Form1.Designer.cs
@@ -42,11 +42,12 @@
             CochranButton = new RadioButton();
             SlovinButton = new RadioButton();
             groupBox1 = new GroupBox();
+            PlusMinLabel1 = new Label();
+            ZSComboBox = new ComboBox();
+            ZSButton2 = new RadioButton();
+            ZSTextBox = new TextBox();
+            ZSButton1 = new RadioButton();
             label1 = new Label();
-            comboBox1 = new ComboBox();
-            radioButton1 = new RadioButton();
-            textBox2 = new TextBox();
-            radioButton2 = new RadioButton();
             PSGroup.SuspendLayout();
             MEGroup.SuspendLayout();
             FGroup.SuspendLayout();
@@ -63,8 +64,9 @@
             PSTextBox.Name = "PSTextBox";
             PSTextBox.Size = new Size(46, 23);
             PSTextBox.TabIndex = 0;
+            PSTextBox.Text = "457";
             PSTextBox.TextAlign = HorizontalAlignment.Right;
-            PSTextBox.KeyPress += PSTextBox_KeyPress;
+            PSTextBox.KeyPress += ShouldBlockKeyPress;
             // 
             // PSButton1
             // 
@@ -74,7 +76,7 @@
             PSButton1.Size = new Size(14, 13);
             PSButton1.TabIndex = 1;
             PSButton1.UseVisualStyleBackColor = true;
-            PSButton1.CheckedChanged += PSButton1_CheckedChanged;
+            PSButton1.CheckedChanged += OnRadioButtonCheck;
             // 
             // PSGroup
             // 
@@ -82,7 +84,7 @@
             PSGroup.Controls.Add(PSTextBox);
             PSGroup.Controls.Add(PSButton1);
             PSGroup.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            PSGroup.Location = new Point(163, 38);
+            PSGroup.Location = new Point(163, 39);
             PSGroup.Name = "PSGroup";
             PSGroup.Size = new Size(110, 103);
             PSGroup.TabIndex = 2;
@@ -110,7 +112,7 @@
             MEGroup.Controls.Add(METextBox);
             MEGroup.Controls.Add(MEButton1);
             MEGroup.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            MEGroup.Location = new Point(289, 38);
+            MEGroup.Location = new Point(289, 39);
             MEGroup.Name = "MEGroup";
             MEGroup.Size = new Size(110, 103);
             MEGroup.TabIndex = 3;
@@ -149,7 +151,7 @@
             MEButton2.TabIndex = 3;
             MEButton2.TabStop = true;
             MEButton2.UseVisualStyleBackColor = true;
-            MEButton2.CheckedChanged += MEButton2_CheckedChanged;
+            MEButton2.CheckedChanged += OnRadioButtonCheck;
             // 
             // METextBox
             // 
@@ -161,8 +163,9 @@
             METextBox.Name = "METextBox";
             METextBox.Size = new Size(31, 23);
             METextBox.TabIndex = 0;
+            METextBox.Text = "3";
             METextBox.TextAlign = HorizontalAlignment.Right;
-            METextBox.KeyPress += METextBox_KeyPress;
+            METextBox.KeyPress += ShouldBlockKeyPress;
             // 
             // MEButton1
             // 
@@ -172,7 +175,7 @@
             MEButton1.Size = new Size(14, 13);
             MEButton1.TabIndex = 1;
             MEButton1.UseVisualStyleBackColor = true;
-            MEButton1.CheckedChanged += MEButton1_CheckedChanged;
+            MEButton1.CheckedChanged += OnRadioButtonCheck;
             // 
             // FGroup
             // 
@@ -198,7 +201,7 @@
             CochranButton.TabStop = true;
             CochranButton.Text = "Cochran";
             CochranButton.UseVisualStyleBackColor = true;
-            CochranButton.CheckedChanged += CochranButton_CheckedChanged;
+            CochranButton.CheckedChanged += UpdateFormula;
             // 
             // SlovinButton
             // 
@@ -210,76 +213,90 @@
             SlovinButton.TabIndex = 1;
             SlovinButton.Text = "Slovin";
             SlovinButton.UseVisualStyleBackColor = true;
-            SlovinButton.CheckedChanged += SlovinButton_CheckedChanged;
+            SlovinButton.CheckedChanged += UpdateFormula;
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(comboBox1);
-            groupBox1.Controls.Add(radioButton1);
-            groupBox1.Controls.Add(textBox2);
-            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(PlusMinLabel1);
+            groupBox1.Controls.Add(ZSComboBox);
+            groupBox1.Controls.Add(ZSButton2);
+            groupBox1.Controls.Add(ZSTextBox);
+            groupBox1.Controls.Add(ZSButton1);
             groupBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            groupBox1.Location = new Point(417, 38);
+            groupBox1.Location = new Point(416, 39);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(110, 103);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "Z Score";
             // 
+            // PlusMinLabel1
+            // 
+            PlusMinLabel1.AutoSize = true;
+            PlusMinLabel1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PlusMinLabel1.Location = new Point(25, 30);
+            PlusMinLabel1.Name = "PlusMinLabel1";
+            PlusMinLabel1.Size = new Size(15, 15);
+            PlusMinLabel1.TabIndex = 4;
+            PlusMinLabel1.Text = "±";
+            PlusMinLabel1.TextAlign = ContentAlignment.TopRight;
+            // 
+            // ZSComboBox
+            // 
+            ZSComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            ZSComboBox.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ZSComboBox.FormattingEnabled = true;
+            ZSComboBox.Items.AddRange(new object[] { "2.33", "1.96", "1.65" });
+            ZSComboBox.Location = new Point(40, 63);
+            ZSComboBox.Name = "ZSComboBox";
+            ZSComboBox.Size = new Size(46, 23);
+            ZSComboBox.TabIndex = 4;
+            // 
+            // ZSButton2
+            // 
+            ZSButton2.AutoSize = true;
+            ZSButton2.Checked = true;
+            ZSButton2.Location = new Point(11, 67);
+            ZSButton2.Name = "ZSButton2";
+            ZSButton2.Size = new Size(14, 13);
+            ZSButton2.TabIndex = 3;
+            ZSButton2.TabStop = true;
+            ZSButton2.UseVisualStyleBackColor = true;
+            ZSButton2.CheckedChanged += OnRadioButtonCheck;
+            // 
+            // ZSTextBox
+            // 
+            ZSTextBox.BackColor = SystemColors.Window;
+            ZSTextBox.Enabled = false;
+            ZSTextBox.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ZSTextBox.Location = new Point(39, 27);
+            ZSTextBox.MaxLength = 4;
+            ZSTextBox.Name = "ZSTextBox";
+            ZSTextBox.Size = new Size(47, 23);
+            ZSTextBox.TabIndex = 0;
+            ZSTextBox.TextAlign = HorizontalAlignment.Right;
+            ZSTextBox.KeyPress += ShouldBlockKeyPress;
+            // 
+            // ZSButton1
+            // 
+            ZSButton1.AutoSize = true;
+            ZSButton1.Location = new Point(11, 32);
+            ZSButton1.Name = "ZSButton1";
+            ZSButton1.Size = new Size(14, 13);
+            ZSButton1.TabIndex = 1;
+            ZSButton1.UseVisualStyleBackColor = true;
+            ZSButton1.CheckedChanged += OnRadioButtonCheck;
+            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(59, 30);
+            label1.Location = new Point(442, 104);
             label1.Name = "label1";
-            label1.Size = new Size(17, 15);
-            label1.TabIndex = 4;
-            label1.Text = "%";
+            label1.Size = new Size(15, 15);
+            label1.TabIndex = 6;
+            label1.Text = "±";
             label1.TextAlign = ContentAlignment.TopRight;
-            // 
-            // comboBox1
-            // 
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "2%", "5%", "10%" });
-            comboBox1.Location = new Point(30, 63);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(46, 23);
-            comboBox1.TabIndex = 4;
-            // 
-            // radioButton1
-            // 
-            radioButton1.AutoSize = true;
-            radioButton1.Checked = true;
-            radioButton1.Location = new Point(11, 67);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(14, 13);
-            radioButton1.TabIndex = 3;
-            radioButton1.TabStop = true;
-            radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            textBox2.BackColor = SystemColors.Window;
-            textBox2.Enabled = false;
-            textBox2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(30, 27);
-            textBox2.MaxLength = 3;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(31, 23);
-            textBox2.TabIndex = 0;
-            textBox2.TextAlign = HorizontalAlignment.Right;
-            // 
-            // radioButton2
-            // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(11, 32);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(14, 13);
-            radioButton2.TabIndex = 1;
-            radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -287,13 +304,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             ClientSize = new Size(784, 561);
+            Controls.Add(label1);
             Controls.Add(groupBox1);
             Controls.Add(FGroup);
             Controls.Add(MEGroup);
             Controls.Add(PSGroup);
             MaximizeBox = false;
             Name = "Form1";
-            Text = "Sample Size Simulator";
+            Text = "-=";
             PSGroup.ResumeLayout(false);
             PSGroup.PerformLayout();
             MEGroup.ResumeLayout(false);
@@ -303,6 +321,7 @@
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -322,10 +341,11 @@
         private TextBox textBox1;
         private RadioButton SlovinButton;
         private GroupBox groupBox1;
+        private Label PlusMinLabel1;
+        private ComboBox ZSComboBox;
+        private RadioButton ZSButton2;
+        private TextBox ZSTextBox;
+        private RadioButton ZSButton1;
         private Label label1;
-        private ComboBox comboBox1;
-        private RadioButton radioButton1;
-        private TextBox textBox2;
-        private RadioButton radioButton2;
     }
 }
--- Sample Size Sim/Form1.cs
+++ Sample Size Sim/Form1.cs
@@ -11,48 +11,59 @@ namespace Sample_Size_Sim
         public Form1()
         {
             InitializeComponent();
-            MEComboBox.SelectedIndex = 1;
+            Int32 SECOND_ITEM_INDEX = 1;
+            MEComboBox.SelectedIndex = SECOND_ITEM_INDEX;
+            ZSComboBox.SelectedIndex = SECOND_ITEM_INDEX;
         }
 
-        private static bool ShouldBlockKeyPress(object sender, KeyPressEventArgs e)
+        private void OnRadioButtonCheck(object sender, EventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                switch (radioButton.Name)
+                {
+                    case "PSButton1":
+                        PSTextBox.Enabled = radioButton.Checked;
+                        break;
+                    case "MEButton1":
+                        METextBox.Enabled = radioButton.Checked;
+                        break;
+                    case "MEButton2":
+                        MEComboBox.Enabled = radioButton.Checked;
+                        break;
+                    case "ZSButton1":
+                        ZSTextBox.Enabled = radioButton.Checked;
+                        break;
+                    case "ZSButton2":
+                        ZSComboBox.Enabled = radioButton.Checked;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void UpdateFormula(object sender, EventArgs e)
+        {
+            formula = this.CochranButton.Checked ? Formula.Cochran : Formula.Slovin;
+        }
+ 
+        private void ShouldBlockKeyPress(object sender, KeyPressEventArgs e)
         {
             String text = ((TextBox)sender).Text;
 
             if (char.IsControl(e.KeyChar) || char.IsDigit(e.KeyChar))
             {
-                return false;
+                e.Handled = false;
             }
-            if (e.KeyChar == '.' && !(text.Contains('.')))
+            else if (e.KeyChar == '.' && !(text.Contains('.')))
             {
-                return false;
+                e.Handled = false;
             }
-
-            return true;
-        }
-
-        private void PSTextBox_KeyPress(object sender, KeyPressEventArgs e) => e.Handled = ShouldBlockKeyPress(sender, e);
-
-        private void PSButton1_CheckedChanged(object sender, EventArgs e) => PSTextBox.Enabled = PSButton1.Checked;
-        private void MEButton1_CheckedChanged(object sender, EventArgs e) => METextBox.Enabled = MEButton1.Checked;
-        private void MEButton2_CheckedChanged(object sender, EventArgs e) => MEComboBox.Enabled = MEButton2.Checked;
-        
-        private void METextBox_KeyPress(object sender, KeyPressEventArgs e) => e.Handled = ShouldBlockKeyPress(sender, e);
-
-        private void SetFormula(Formula option)
-        {
-            formula = option;
-        }
-
-        private void SlovinButton_CheckedChanged(object sender, EventArgs e)
-        {
-            Formula f = this.SlovinButton.Checked ? Formula.Slovin : Formula.Cochran;
-            SetFormula(f);
-        }
-
-        private void CochranButton_CheckedChanged(object sender, EventArgs e)
-        {
-            Formula f = this.CochranButton.Checked ? Formula.Cochran : Formula.Slovin;
-            SetFormula(f);
+            else
+            {
+                e.Handled = true;
+            }
         }
     }
 }
